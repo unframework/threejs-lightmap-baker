22e70bb62d80e103401e6c2e801a424ac1f97a92
Nick Matantsev <nick.matantsev@gmail.com>
2020-05-03 23:08:10 -0400

    Playing with shadow casting

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import React, { useRef, useState } from 'react';
 import { Canvas, useFrame, ReactThreeFiber } from 'react-three-fiber';
 import * as THREE from 'three';
 
-const Box: React.FC<{ position: THREE.Vector3 }> = ({ position }) => {
+const Box: React.FC<{ position: [number, number, number] }> = ({
+  position
+}) => {
   // This reference will give us direct access to the mesh
   const meshRef = useRef<ReactThreeFiber.Object3DNode<THREE.Geometry, []>>();
 
@@ -28,7 +30,9 @@ const Box: React.FC<{ position: THREE.Vector3 }> = ({ position }) => {
     <mesh
       ref={meshRef}
       position={position}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      scale={active ? [2.5, 2.5, 2.5] : [1, 1, 1]}
+      castShadow
+      receiveShadow
       onClick={(e) => setActive(!active)}
       onPointerOver={(e) => setHover(true)}
       onPointerOut={(e) => setHover(false)}
@@ -44,9 +48,27 @@ const Box: React.FC<{ position: THREE.Vector3 }> = ({ position }) => {
 
 function App() {
   return (
-    <Canvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
+    <Canvas
+      shadowMap
+      onCreated={({ gl }) => {
+        gl.toneMapping = THREE.ACESFilmicToneMapping;
+        gl.outputEncoding = THREE.sRGBEncoding;
+      }}
+    >
+      >
+      <ambientLight intensity={0.2} />
+      <pointLight position={[30, 30, 30]} intensity={0.4} />
+      <spotLight
+        intensity={0.3}
+        position={[30, 7, 40]}
+        angle={0.1}
+        penumbra={0.4}
+        castShadow
+      />
+      <mesh receiveShadow position={[0, 0, -2]}>
+        <planeBufferGeometry attach="geometry" args={[100, 100]} />
+        <meshStandardMaterial attach="material" color="#171717" />
+      </mesh>
       <Box position={[-1.2, 0, 0]} />
       <Box position={[1.2, 0, 0]} />
     </Canvas>
