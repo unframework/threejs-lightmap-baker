b0c8e8c82fb80ec332dc3a3a10490f313937a3ec
Nick Matantsev <nick.matantsev@gmail.com>
2020-05-03 23:14:47 -0400

    Set up scene for AO experiments

--- src/App.tsx
+++ src/App.tsx
@@ -2,62 +2,19 @@ import React, { useRef, useState } from 'react';
 import { Canvas, useFrame, ReactThreeFiber } from 'react-three-fiber';
 import * as THREE from 'three';
 
-const Box: React.FC<{ position: [number, number, number] }> = ({
-  position
-}) => {
-  // This reference will give us direct access to the mesh
-  const meshRef = useRef<ReactThreeFiber.Object3DNode<THREE.Geometry, []>>();
-
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-
-  // Rotate mesh every frame, this is outside of React without overhead
-  useFrame(() => {
-    if (!meshRef.current) {
-      return;
-    }
-
-    const meshRotation = meshRef.current.rotation;
-    if (!meshRotation) {
-      return;
-    }
-
-    meshRotation.x = meshRotation.y += 0.01;
-  });
-
-  return (
-    <mesh
-      ref={meshRef}
-      position={position}
-      scale={active ? [2.5, 2.5, 2.5] : [1, 1, 1]}
-      castShadow
-      receiveShadow
-      onClick={(e) => setActive(!active)}
-      onPointerOver={(e) => setHover(true)}
-      onPointerOut={(e) => setHover(false)}
-    >
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshStandardMaterial
-        attach="material"
-        color={hovered ? 'hotpink' : 'orange'}
-      />
-    </mesh>
-  );
-};
-
 function App() {
   return (
     <Canvas
       shadowMap
+      camera={{ position: [-4, -4, 8], up: [0, 0, 1] }}
       onCreated={({ gl }) => {
         gl.toneMapping = THREE.ACESFilmicToneMapping;
         gl.outputEncoding = THREE.sRGBEncoding;
       }}
     >
       >
-      <ambientLight intensity={0.2} />
-      <pointLight position={[30, 30, 30]} intensity={0.4} />
+      <ambientLight intensity={0.1} />
+      <pointLight position={[-30, 30, 30]} intensity={0.2} />
       <spotLight
         intensity={0.3}
         position={[30, 7, 40]}
@@ -65,12 +22,18 @@ function App() {
         penumbra={0.4}
         castShadow
       />
-      <mesh receiveShadow position={[0, 0, -2]}>
-        <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <mesh receiveShadow position={[0, 0, -5]}>
+        <planeBufferGeometry attach="geometry" args={[200, 200]} />
         <meshStandardMaterial attach="material" color="#171717" />
       </mesh>
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+      <mesh position={[0, 0, -2]} castShadow receiveShadow>
+        <boxBufferGeometry attach="geometry" args={[5, 5, 2]} />
+        <meshStandardMaterial attach="material" color="orange" />
+      </mesh>
+      <mesh position={[0, 0, 2]} castShadow receiveShadow>
+        <boxBufferGeometry attach="geometry" args={[2, 2, 5]} />
+        <meshStandardMaterial attach="material" color="hotpink" />
+      </mesh>
     </Canvas>
   );
 }
