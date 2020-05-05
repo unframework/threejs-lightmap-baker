0e2474f4ec55a3956a777b7c24bba98f65f865ce
Nick Matantsev <nick.matantsev@gmail.com>
2020-05-04 22:49:11 -0400

    Use ortho debug texture display

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Canvas, useResource, useFrame } from 'react-three-fiber';
+import React, { useMemo } from 'react';
+import { Canvas, useResource, useFrame, useThree } from 'react-three-fiber';
 import * as THREE from 'three';
 
 import { useAtlas, useMeshWithAtlas } from './Atlas';
@@ -15,6 +15,14 @@ function Scene() {
   } = useAtlas();
 
   const [mainSceneRef, mainScene] = useResource<THREE.Scene>();
+  const [debugSceneRef, debugScene] = useResource<THREE.Scene>();
+
+  const { size } = useThree();
+  const debugCamera = useMemo(() => {
+    // top-left corner is (0, 100), top-right is (100, 100)
+    const aspect = size.height / size.width;
+    return new THREE.OrthographicCamera(0, 100, 100, 100 * (1 - aspect), -1, 1);
+  }, [size]);
 
   const [meshBuffer1Ref, meshBuffer1] = useResource<THREE.BufferGeometry>();
   const mesh1Ref = useMeshWithAtlas(atlasInfo, meshBuffer1);
@@ -29,16 +37,30 @@ function Scene() {
     gl.render(mainScene, camera);
   }, 20);
 
+  useFrame(({ gl }) => {
+    gl.autoClear = false;
+    gl.clearDepth();
+    gl.render(debugScene, debugCamera);
+    gl.autoClear = true;
+  }, 30);
+
   return (
     <>
+      <scene ref={debugSceneRef}>
+        <mesh position={[10, 90, 0]}>
+          <planeBufferGeometry attach="geometry" args={[10, 10]} />
+          <meshBasicMaterial attach="material" map={probeDebugTexture} />
+        </mesh>
+        <mesh position={[85, 85, 0]}>
+          <planeBufferGeometry attach="geometry" args={[20, 20]} />
+          <meshBasicMaterial attach="material" map={outputTexture} />
+        </mesh>
+      </scene>
+
       <scene ref={mainSceneRef}>
         <mesh position={[0, 0, -5]}>
           <planeBufferGeometry attach="geometry" args={[200, 200]} />
           <meshBasicMaterial attach="material" color="#171717" />
-        </mesh>
-        <mesh position={[-4, 4, 0]}>
-          <planeBufferGeometry attach="geometry" args={[2, 2]} />
-          <meshBasicMaterial attach="material" map={probeDebugTexture} />
         </mesh>
 
         <mesh position={[0, 0, -1]} ref={mesh1Ref}>
