40449d15cd2fcd013d8945263d7d3396b1e29cb2
Nick Matantsev <nick.matantsev@gmail.com>
2020-05-06 21:28:13 -0400

    Move irradiance-aware materials into dedicated file

--- src/App.tsx
+++ src/App.tsx
@@ -8,148 +8,13 @@ import IrradianceSurfaceManager, {
 import { useIrradianceRenderer } from './IrradianceRenderer';
 import SceneControls from './SceneControls';
 import GridGeometry from './GridGeometry';
-
-const ProbeLightMaterial: React.FC<{ attach?: string; intensity: number }> = ({
-  attach,
-  intensity
-}) => {
-  const material = new THREE.ShaderMaterial({
-    uniforms: {
-      color: { value: new THREE.Color(0xffffff) },
-      intensity: { value: 1 }
-    },
-    vertexShader: `
-      void main() {
-        vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
-        gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-      }
-    `,
-    fragmentShader: `
-      uniform vec3 color;
-      uniform float intensity;
-
-      void main() {
-        gl_FragColor = vec4( color * intensity, 1.0 );
-      }
-    `
-  });
-
-  // disposable managed object
-  return (
-    <primitive
-      object={material}
-      attach={attach}
-      uniforms-intensity-value={intensity}
-    />
-  );
-};
-
-const ProbeMeshMaterial: React.FC<{
-  attach?: string;
-  lumMap: THREE.Texture;
-}> = ({ attach, lumMap }) => {
-  const material = new THREE.ShaderMaterial({
-    uniforms: {
-      lum: { value: null }
-    },
-
-    vertexShader: `
-      attribute vec2 lumUV;
-      varying vec2 vUV;
-
-      void main() {
-        vUV = lumUV;
-
-        vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
-        gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-      }
-    `,
-    fragmentShader: `
-      uniform sampler2D lum;
-      varying vec2 vUV;
-
-      void main() {
-        gl_FragColor = texture2D(lum, vUV);
-      }
-    `
-  });
-
-  // disposable managed object
-  return (
-    <primitive object={material} attach={attach} uniforms-lum-value={lumMap} />
-  );
-};
-
-const ProbeDebugMaterial: React.FC<{
-  attach?: string;
-  lumMap: THREE.Texture;
-}> = ({ attach, lumMap }) => {
-  const material = new THREE.ShaderMaterial({
-    uniforms: {
-      lum: { value: null }
-    },
-
-    vertexShader: `
-      varying vec2 vUV;
-
-      void main() {
-        vUV = uv;
-
-        vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
-        gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-      }
-    `,
-    fragmentShader: `
-      uniform sampler2D lum;
-      varying vec2 vUV;
-
-      void main() {
-        gl_FragColor = texture2D(lum, vUV);
-      }
-    `
-  });
-
-  // disposable managed object
-  return (
-    <primitive object={material} attach={attach} uniforms-lum-value={lumMap} />
-  );
-};
-
-const FinalMeshMaterial: React.FC<{
-  attach?: string;
-  lumMap: THREE.Texture;
-}> = ({ attach, lumMap }) => {
-  const material = new THREE.ShaderMaterial({
-    uniforms: {
-      lum: { value: null }
-    },
-
-    vertexShader: `
-      attribute vec2 lumUV;
-      varying vec2 vUV;
-
-      void main() {
-        vUV = lumUV;
-
-        vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
-        gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-      }
-    `,
-    fragmentShader: `
-      uniform sampler2D lum;
-      varying vec2 vUV;
-
-      void main() {
-        gl_FragColor = vec4(toneMapping(texture2D(lum, vUV).rgb), 1.0);
-      }
-    `
-  });
-
-  // disposable managed object
-  return (
-    <primitive object={material} attach={attach} uniforms-lum-value={lumMap} />
-  );
-};
+import {
+  ProbeLightMaterial,
+  ProbeMeshMaterial,
+  FinalMeshMaterial,
+  IrradianceDebugMaterial,
+  IrradianceTextureContext
+} from './IrradianceMaterials';
 
 function Scene() {
   const {
@@ -200,50 +65,60 @@ function Scene() {
         {probeDebugTextures.map((tex, texIndex) => (
           <mesh position={[5, 95 - texIndex * 9, 0]} key={texIndex}>
             <planeBufferGeometry attach="geometry" args={[8, 8]} />
-            <ProbeDebugMaterial attach="material" lumMap={tex} />
+            <IrradianceDebugMaterial attach="material" lumMap={tex} />
           </mesh>
         ))}
         <mesh position={[85, 85, 0]}>
           <planeBufferGeometry attach="geometry" args={[20, 20]} />
-          <ProbeDebugMaterial attach="material" lumMap={outputTexture} />
+          <IrradianceDebugMaterial attach="material" lumMap={outputTexture} />
         </mesh>
       </scene>
 
-      <scene ref={mainSceneRef}>
-        <mesh position={[0, 0, -5]}>
-          <planeBufferGeometry attach="geometry" args={[200, 200]} />
-          <meshBasicMaterial attach="material" color="#171717" />
-        </mesh>
+      <IrradianceTextureContext.Provider value={outputTexture}>
+        <scene ref={mainSceneRef}>
+          <mesh position={[0, 0, -5]}>
+            <planeBufferGeometry attach="geometry" args={[200, 200]} />
+            <meshBasicMaterial attach="material" color="#171717" />
+          </mesh>
 
-        <mesh position={[0, 0, -1]} ref={mesh1Ref} onClick={handleDebugClick}>
-          <GridGeometry attach="geometry" ref={meshBuffer1Ref} />
-          <FinalMeshMaterial attach="material" lumMap={outputTexture} />
-        </mesh>
-        <mesh position={[-1.5, 0, 2]} ref={mesh2Ref} onClick={handleDebugClick}>
-          <boxBufferGeometry
-            attach="geometry"
-            args={[2, 1, 4.5]}
-            ref={meshBuffer2Ref}
-          />
-          <FinalMeshMaterial attach="material" lumMap={outputTexture} />
-        </mesh>
-        <mesh position={[1.5, 0, 2]} ref={mesh3Ref} onClick={handleDebugClick}>
-          <boxBufferGeometry
-            attach="geometry"
-            args={[2, 1, 4.5]}
-            ref={meshBuffer3Ref}
-          />
-          <FinalMeshMaterial attach="material" lumMap={outputTexture} />
-        </mesh>
-        <mesh position={[0, 3, 3]} ref={mesh4Ref} onClick={handleDebugClick}>
-          <boxBufferGeometry
-            attach="geometry"
-            args={[3, 0.5, 3]}
-            ref={meshBuffer4Ref}
-          />
-          <FinalMeshMaterial attach="material" lumMap={outputTexture} />
-        </mesh>
-      </scene>
+          <mesh position={[0, 0, -1]} ref={mesh1Ref} onClick={handleDebugClick}>
+            <GridGeometry attach="geometry" ref={meshBuffer1Ref} />
+            <FinalMeshMaterial attach="material" />
+          </mesh>
+          <mesh
+            position={[-1.5, 0, 2]}
+            ref={mesh2Ref}
+            onClick={handleDebugClick}
+          >
+            <boxBufferGeometry
+              attach="geometry"
+              args={[2, 1, 4.5]}
+              ref={meshBuffer2Ref}
+            />
+            <FinalMeshMaterial attach="material" />
+          </mesh>
+          <mesh
+            position={[1.5, 0, 2]}
+            ref={mesh3Ref}
+            onClick={handleDebugClick}
+          >
+            <boxBufferGeometry
+              attach="geometry"
+              args={[2, 1, 4.5]}
+              ref={meshBuffer3Ref}
+            />
+            <FinalMeshMaterial attach="material" />
+          </mesh>
+          <mesh position={[0, 3, 3]} ref={mesh4Ref} onClick={handleDebugClick}>
+            <boxBufferGeometry
+              attach="geometry"
+              args={[3, 0.5, 3]}
+              ref={meshBuffer4Ref}
+            />
+            <FinalMeshMaterial attach="material" />
+          </mesh>
+        </scene>
+      </IrradianceTextureContext.Provider>
 
       <scene ref={lightSceneRef}>
         {mesh1Ref.current && meshBuffer1 && (
---
+++ src/IrradianceMaterials.tsx
@@ -0,0 +1,160 @@
+import React, { useContext } from 'react';
+import * as THREE from 'three';
+
+// @todo wrap in provider helper
+export const IrradianceTextureContext = React.createContext<THREE.Texture | null>(
+  null
+);
+
+function useIrradianceTexture() {
+  const texture = useContext(IrradianceTextureContext);
+
+  if (!texture) {
+    throw new Error('no texture provided');
+  }
+
+  return texture;
+}
+
+export const ProbeLightMaterial: React.FC<{
+  attach?: string;
+  intensity: number;
+}> = ({ attach, intensity }) => {
+  const material = new THREE.ShaderMaterial({
+    uniforms: {
+      color: { value: new THREE.Color(0xffffff) },
+      intensity: { value: 1 }
+    },
+    vertexShader: `
+      void main() {
+        vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
+        gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+      }
+    `,
+    fragmentShader: `
+      uniform vec3 color;
+      uniform float intensity;
+
+      void main() {
+        gl_FragColor = vec4( color * intensity, 1.0 );
+      }
+    `
+  });
+
+  // disposable managed object
+  return (
+    <primitive
+      object={material}
+      attach={attach}
+      uniforms-intensity-value={intensity}
+    />
+  );
+};
+
+export const ProbeMeshMaterial: React.FC<{
+  attach?: string;
+  lumMap: THREE.Texture;
+}> = ({ attach, lumMap }) => {
+  const material = new THREE.ShaderMaterial({
+    uniforms: {
+      lum: { value: null }
+    },
+
+    vertexShader: `
+      attribute vec2 lumUV;
+      varying vec2 vUV;
+
+      void main() {
+        vUV = lumUV;
+
+        vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
+        gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+      }
+    `,
+    fragmentShader: `
+      uniform sampler2D lum;
+      varying vec2 vUV;
+
+      void main() {
+        gl_FragColor = texture2D(lum, vUV);
+      }
+    `
+  });
+
+  // disposable managed object
+  return (
+    <primitive object={material} attach={attach} uniforms-lum-value={lumMap} />
+  );
+};
+
+export const FinalMeshMaterial: React.FC<{
+  attach?: string;
+}> = ({ attach }) => {
+  const lumMap = useIrradianceTexture();
+
+  const material = new THREE.ShaderMaterial({
+    uniforms: {
+      lum: { value: null }
+    },
+
+    vertexShader: `
+      attribute vec2 lumUV;
+      varying vec2 vUV;
+
+      void main() {
+        vUV = lumUV;
+
+        vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
+        gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+      }
+    `,
+    fragmentShader: `
+      uniform sampler2D lum;
+      varying vec2 vUV;
+
+      void main() {
+        gl_FragColor = vec4(toneMapping(texture2D(lum, vUV).rgb), 1.0);
+      }
+    `
+  });
+
+  // disposable managed object
+  return (
+    <primitive object={material} attach={attach} uniforms-lum-value={lumMap} />
+  );
+};
+
+export const IrradianceDebugMaterial: React.FC<{
+  attach?: string;
+  lumMap: THREE.Texture;
+}> = ({ attach, lumMap }) => {
+  const material = new THREE.ShaderMaterial({
+    uniforms: {
+      lum: { value: null }
+    },
+
+    vertexShader: `
+      varying vec2 vUV;
+
+      void main() {
+        vUV = uv;
+
+        vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
+        gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+      }
+    `,
+    fragmentShader: `
+      uniform sampler2D lum;
+      varying vec2 vUV;
+
+      void main() {
+        gl_FragColor = texture2D(lum, vUV);
+      }
+    `
+  });
+
+  // disposable managed object
+  return (
+    <primitive object={material} attach={attach} uniforms-lum-value={lumMap} />
+  );
+};
