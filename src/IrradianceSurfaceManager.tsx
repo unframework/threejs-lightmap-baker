9bbe426035e78f071e54fbb6d93e91edb6cee5f1
Nick Matantsev <nick.matantsev@gmail.com>
2020-10-11 03:20:37 -0300

    Update scene with better geometry and UV2 unwrap

--- src/IrradianceSurfaceManager.tsx
+++ src/IrradianceSurfaceManager.tsx
@@ -2,8 +2,8 @@ import React, { useMemo, useCallback, useContext } from 'react';
 import { useUpdate } from 'react-three-fiber';
 import * as THREE from 'three';
 
-export const atlasWidth = 128;
-export const atlasHeight = 128;
+export const atlasWidth = 64;
+export const atlasHeight = 64;
 
 const bleedOffsetU = 1 / atlasWidth;
 const bleedOffsetV = 1 / atlasHeight;
