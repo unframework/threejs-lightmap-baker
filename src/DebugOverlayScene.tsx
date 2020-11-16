829b2876decf208075bd70d355a61b430862e022
Nick Matantsev <nick.matantsev@gmail.com>
2020-11-16 17:55:09 -0500

    Use meshBasicMaterial for debug display

--- src/DebugOverlayScene.tsx
+++ src/DebugOverlayScene.tsx
@@ -5,47 +5,6 @@ import * as THREE from 'three';
 import { useIrradianceTexture } from './core/IrradianceCompositor';
 import { PROBE_BATCH_COUNT } from './core/IrradianceLightProbe';
 
-// simple debug material that skips tone mapping
-// @todo replace with meshBasicMaterial (currently latter shows up brighter even with toneMapped = false)
-const DebugMaterial: React.FC<{
-  attach?: string;
-  map: THREE.Texture;
-}> = ({ attach, map }) => {
-  const material = useMemo(
-    () =>
-      new THREE.ShaderMaterial({
-        uniforms: {
-          map: { value: null }
-        },
-
-        vertexShader: `
-          varying vec2 vUV;
-
-          void main() {
-            vUV = uv;
-
-            vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
-            gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-          }
-        `,
-        fragmentShader: `
-          uniform sampler2D map;
-          varying vec2 vUV;
-
-          void main() {
-            gl_FragColor = texture2D(map, vUV);
-          }
-        `
-      }),
-    []
-  );
-
-  // disposable managed object
-  return (
-    <primitive object={material} attach={attach} uniforms-map-value={map} />
-  );
-};
-
 export const DebugOverlayScene: React.FC<{
   atlasTexture?: THREE.Texture | null;
   probeTexture?: THREE.Texture | null;
@@ -73,14 +32,22 @@ export const DebugOverlayScene: React.FC<{
       {outputTexture && (
         <mesh position={[85, 85, 0]}>
           <planeBufferGeometry attach="geometry" args={[20, 20]} />
-          <DebugMaterial attach="material" map={outputTexture} />
+          <meshBasicMaterial
+            attach="material"
+            map={outputTexture}
+            toneMapped={false}
+          />
         </mesh>
       )}
 
       {atlasTexture && (
         <mesh position={[85, 64, 0]}>
           <planeBufferGeometry attach="geometry" args={[20, 20]} />
-          <DebugMaterial attach="material" map={atlasTexture} />
+          <meshBasicMaterial
+            attach="material"
+            map={atlasTexture}
+            toneMapped={false}
+          />
         </mesh>
       )}
 
@@ -90,7 +57,11 @@ export const DebugOverlayScene: React.FC<{
             attach="geometry"
             args={[10, 5 * PROBE_BATCH_COUNT]}
           />
-          <DebugMaterial attach="material" map={probeTexture} />
+          <meshBasicMaterial
+            attach="material"
+            map={probeTexture}
+            toneMapped={false}
+          />
         </mesh>
       )}
     </scene>
