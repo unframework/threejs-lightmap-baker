c43a7c1594614ccd556650142b1f951cee80c799
Nick Matantsev <nick.matantsev@gmail.com>
2020-11-17 15:27:44 -0500

    Install Storybook

---
+++ src/stories/gltf.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+
+export default {
+  title: 'glTF scene'
+} as Meta;
+
+export const Main: Story = () => <div>TBD</div>;
